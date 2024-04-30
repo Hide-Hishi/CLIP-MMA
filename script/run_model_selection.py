2af6b1a1f816578ab3fcc616f49e817981638ed3
Hide-Hishi <79154294+Hide-Hishi@users.noreply.github.com>
2024-05-01 07:28:29 +0900

    Add files via upload

---
+++ script/run_model_selection.py
@@ -0,0 +1,45 @@
+import yaml
+import clip
+import torch
+import pickle
+import os
+from PIL import Image, ImageFilter
+import glob
+import sys
+from pathlib import Path
+import argparse
+
+
+base_dir = Path(os.path.abspath(__file__)).parents[1]
+sys.path.insert(0, base_dir)
+
+from umap_mma.selection_func import measure_similarity
+
+
+SAVE_DIR = "results"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Process an image.")
+    parser.add_argument("cfg_filename", type=str, default= "config.yaml", help="Path to the config file you want to use")
+    parser.add_argument("img_path", type=str, help="Path to the image file")
+
+    args = parser.parse_args()
+
+    cfg_name = args.cfg_filename
+    yaml_file_path = os.path.join(base_dir, 'config',cfg_name)
+
+    with open(yaml_file_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    img_path = args.img_path
+
+    pil_img = Image.open(img_path)
+    blur_img = pil_img.filter(ImageFilter.GaussianBlur(radius=3))
+
+    os.makedirs(SAVE_DIR,exist_ok=True)
+    measure_similarity(blur_img, config, SAVE_DIR, auto_crop=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ script/run_parameter_estimation.py
@@ -0,0 +1,55 @@
+import yaml
+import clip
+import torch
+import pickle
+import os
+from PIL import Image
+import glob
+import sys
+import numpy as np
+from PIL import Image
+import glob
+from pathlib import Path
+from pathlib import Path
+import argparse
+
+base_dir = Path(os.path.abspath(__file__)).parents[1]
+sys.path.insert(0, base_dir)
+
+from clip_mma.estimation import parameter_estimation, show_estimation2D
+
+def target_param_extractor(img_path):
+    filename = os.path.basename(img_path)
+    fv = float(filename.split("_")[1])
+    gv = float(filename.split("_")[3])
+    return [fv,gv]
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Process an image.")
+    parser.add_argument("cfg_filename", type=str, default= "config.yaml", help="Path to the config file you want to use")
+    parser.add_argument("target_images_path", type=str)
+
+    args = parser.parse_args()
+
+    cfg_name = args.cfg_filename
+    yaml_file_path = os.path.join(base_dir, 'config',cfg_name)
+
+    with open(yaml_file_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    #custom how to extract ground truth parameters of the target
+    target_images_path = args.target_images_path
+    gt_param = target_param_extractor(os.path.basename(target_images_path))
+
+    pil_images = []
+    for img_path in glob.glob(os.path.join(target_images_path,"*.png")):
+        pil_img = Image.open(img_path)
+        pil_images.append(pil_img)
+
+    param_pred = parameter_estimation(config,pil_images)
+    show_estimation2D(config,param_pred,gt_param)
+
+
+if __name__ == "__main__":
+    main()
