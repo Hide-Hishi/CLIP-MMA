d353b2affa71a4b01e72da518f428aeea61b71b2
Hide-Hishi <79154294+Hide-Hishi@users.noreply.github.com>
2024-05-02 11:16:37 +0900

    Delete clip_mma directory

--- clip_mma/utils.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def find_gpu():
-    if torch.cuda.is_available():
-        return torch.device("cuda")
-    elif torch.backends.mps.is_available():
-        return torch.device("mps")
-    else:
-        return torch.device("cpu")
